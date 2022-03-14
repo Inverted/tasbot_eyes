eb2e0501c9a58a15aee93a64bcd0b6d04e23aa4e
Janisch, Mirco <mirco.janisch@stud.h-da.de>
2022-03-15 00:59:12 +0100

    testing

---
+++ cmake-build-remote-host/CMakeFiles/tasbot_eyes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tasbot_eyes.dir/main.c.o"
+  "tasbot_eyes"
+  "tasbot_eyes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tasbot_eyes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
