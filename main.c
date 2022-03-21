dbe277cee93cd9b493f5521683bc4b5bb8bd36b9
Janisch, Mirco <mirco.janisch@stud.h-da.de>
2022-03-21 09:53:26 -0700

    transfer

--- main.c
+++ main.c
@@ -152,8 +152,8 @@ int main() {
         }
     }
 
-    for (int y = 0; y < LED_WIDTH; ++y) {
-        for (int x = 0; x < LED_HEIGHT; ++x) {
+    for (int y = 0; y < LED_HEIGHT; ++y) {
+        for (int x = 0; x < LED_WIDTH; ++x) {
             pixel[x * LED_HEIGHT + y] = colors[0];
 
             //scanf("%d", &test);
