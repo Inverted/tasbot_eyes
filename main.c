9fc7e82d0c510c21ba2436f979bbaf90d0ce305b
Janisch, Mirco <mirco.janisch@stud.h-da.de>
2022-03-21 10:16:06 -0700

    transfer

--- main.c
+++ main.c
@@ -168,7 +168,11 @@ int main() {
 
     for (int y = 0; y < LED_HEIGHT; ++y) {
         for (int x = 0; x < LED_WIDTH; ++x) {
-            //pixel[x * LED_WIDTH + y] = colors[0];
+
+            int index = TASBotIndex[y][x];
+            if(index >= 0){
+                pixel[x * LED_WIDTH + y] = colors[0];
+            }
 
             scanf("%d", &test);
             //usleep(1000 * 1000);
