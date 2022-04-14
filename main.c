23ba23f0606243bd6caf160c905a609d43c69e1f
Janisch, Mirco <mirco.janisch@stud.h-da.de>
2022-04-14 22:46:19 +0200

    Set build type in CMakeLists.txt

--- main.c
+++ main.c
@@ -737,16 +737,12 @@ ws2811_return_t renderLEDs() {
         }
     }
 
-    printf("now render for real\n");
-
     ws2811_return_t r;
-    if ((r = ws2811_render(&display)) != WS2811_SUCCESS) {
+    if ((r = ws2811_render(&display)) != WS2811_SUCCESS) { //causes issues
         fprintf(stderr, "[ERROR] Failed to render: %s\n", ws2811_get_return_t_str(r));
     } else {
         printf("[INFO] Rendered LEDs with code %d\n", r);
     }
-
-    printf("fail?\n");
 
     return r;
 }
