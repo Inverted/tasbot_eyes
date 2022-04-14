bf07384e6326120d91f84d517e98e3b7f18a8458
Janisch, Mirco <mirco.janisch@stud.h-da.de>
2022-04-14 23:31:00 +0200

    Transferring

--- main.c
+++ main.c
@@ -740,11 +740,6 @@ ws2811_return_t renderLEDs() {
     }
 
     ws2811_return_t r;
-
-    while (true){
-
-    }
-
     if ((r = ws2811_render(&display)) != WS2811_SUCCESS) {
         fprintf(stderr, "[ERROR] Failed to render: %s\n", ws2811_get_return_t_str(r));
     } else {
